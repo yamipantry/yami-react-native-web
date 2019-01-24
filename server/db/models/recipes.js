c9775e8a685d6481999c42af485980dbbe56cf7d
Thomas Weidner <tomweidner103@gmail.com>
2019-01-24 15:55:39 -0800

    seeded db

--- server/db/models/recipes.js
+++ server/db/models/recipes.js
@@ -1,38 +1,50 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const {hash} = require('../../../util')
 
-const Recipes = db.define('recipes', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true
+const Recipes = db.define(
+  'recipes',
+  {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    ingredientsIds: {
+      type: Sequelize.ARRAY(Sequelize.INTEGER),
+      allowNull: false
+    },
+    instructions: {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      allowNull: false
+    },
+    description: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    ingredientAmounts: {
+      type: Sequelize.ARRAY(Sequelize.STRING),
+      allowNull: false
+    },
+    imageUrl: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    hashOutput: {
+      type: Sequelize.BIGINT,
+      // allowNull: false
+      defaultValue: 0
     }
   },
-  ingredientsIds: {
-    type: Sequelize.ARRAY(Sequelize.INTEGER),
-    allowNull: false
-  },
-  instructions: {
-    type: Sequelize.ARRAY(Sequelize.STRING),
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  },
-  ingredientAmounts: {
-    type: Sequelize.ARRAY(Sequelize.STRING),
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  },
-  hashOutput: {
-    type: Sequelize.INTEGER,
-    allowNull: false
+  {
+    hooks: {
+      beforeCreate: recipe => {
+        recipe.hashOutput = hash(recipe.ingredientsIds)
+      }
+    }
   }
-})
+)
 
 module.exports = Recipes
---
+++ util/index.js
@@ -0,0 +1,32 @@
+const intersection = (arrA, arrB) => {
+  const shared = []
+  let idxA = 0
+  let idxB = 0
+  while (idxA < arrA.length && idxB < arrB.length) {
+    const elemA = arrA[idxA]
+    const elemB = arrB[idxB]
+    if (elemA == elemB) {
+      shared.push(elemA)
+    }
+    if (elemA <= elemB) {
+      idxA++
+    }
+    if (elemA >= elemB) {
+      idxB++
+    }
+  }
+  return shared
+}
+
+const hash = ingr => {
+  let list = ingr.sort((a, b) => {
+    return a - b
+  })
+  let int = ingr.length // 5
+  const yoyo = [int, ...list]
+  const joined = yoyo.join('')
+  const number = parseInt(joined, 10)
+  return number
+}
+
+module.exports = {intersection, hash}

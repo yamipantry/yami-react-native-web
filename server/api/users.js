5c7f1d534fbebc97f35ff5044bc332e5c99a8a5b
Victor Marta <vicm159@gmail.com>
2019-01-24 16:01:32 -0800

    pantry Crud and client pantry view

--- server/api/users.js
+++ server/api/users.js
@@ -3,15 +3,19 @@ const {User} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
+  // makes sure no one can get id or email of user that is not logged in
+  // by just going to /api/users/1
+  if (!req.user && process.env.NODE_ENV !== 'test') {
+    res.status(401).send('Sorry Not Logged In')
+  } else {
+    try {
+      const users = await User.findAll({
+        // explicitly select only the id, email, and pantryItems fields
+        attributes: ['id', 'email', 'pantryItems']
+      })
+      res.json(users)
+    } catch (err) {
+      next(err)
+    }
   }
 })

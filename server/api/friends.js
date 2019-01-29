8f3c9f8fc9679804a309f96c5ea259a593a72a76
RNAHVI <rnahvi@hotmail.com>
2019-01-29 15:07:17 -0800

    route added to get a user's friends and the pantry items they have in stock

--- server/api/friends.js
+++ server/api/friends.js
@@ -1,24 +1,23 @@
 const router = require('express').Router()
-const {Userfriends} = require('../db/models')
+const {User} = require('../db/models')
 
 module.exports = router
 
 //GET /api/friends
-router.get('/', async (req, res, next) => {
-  if (!req.user && process.env.NODE_ENV !== 'test') {
-    res.status(401).send('Sorry, try again.  You are not logged in.')
-  } else {
-    try {
-      const friends = await Userfriends.findAll({
-        where: {
-          userId: req.user.id
-        },
-        attributes: ['friendId']
-      })
 
-      res.json(friends)
-    } catch (err) {
-      next(err)
-    }
+app.get('/', async (req, res, next) => {
+  try {
+    const friends = await User.findAll({
+      include: [
+        {model: User, as: 'friends', attributes: ['userName', 'pantryItems']}
+      ],
+      where: {
+        id: req.user.id
+      }
+    })
+
+    res.json(friends)
+  } catch (err) {
+    next(err)
   }
 })

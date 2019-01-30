5f94df3a3499bbeb290bd0c236103f92c48e4419
Ashley Smith <smitham85@gmail.com>
2019-01-29 17:13:56 -0800

    adding api route for updating user info

--- server/api/users.js
+++ server/api/users.js
@@ -36,15 +36,11 @@ router.get('/testSQL', async (req, res, next) => {
 })
 
 router.put('/:userId', async (req, res, next) => {
-  if (!req.user && process.env.NODE_ENV !== 'test') {
-    res.status(401).send('Sorry Not Logged In')
-  } else {
-    try {
-      const user = await User.findById(req.params.userId * 1)
-      let message = await user.update(req.body)
-      res.json(message)
-    } catch (err) {
-      next(err)
-    }
+  try {
+    const user = await User.findById(req.params.userId * 1)
+    let message = await user.update(req.body)
+    res.json(message)
+  } catch (err) {
+    next(err)
   }
 })

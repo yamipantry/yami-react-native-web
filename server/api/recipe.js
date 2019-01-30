c6f5107de7c80744f609c72fac4bc82870ad5677
Omri Bernstein <hippiccolo@gmail.com>
2019-01-30 11:44:35 -0800

    comment (code review one): add feedback

--- server/api/recipe.js
+++ server/api/recipe.js
@@ -3,12 +3,15 @@ const {Recipes, User, Items, Ingredients} = require('../db/models')
 
 router.get('/', async (req, res, next) => {
   try {
+    // OB/MS: great opportunity for a function
     const userPantry = await User.findById(req.user.id, {
+      // OB/MS: might help querying logic if this was an explicit join
       attributes: ['pantryItems']
     })
     const recipes = await Recipes.findIngredients()
     let result = []
     let ingredientsArr = []
+    // OB/MS: don't need to go through .dataValues
     const pantry = userPantry.dataValues.pantryItems
     const recipeToAdd = recipes
     for (let i = 0; i < recipeToAdd.length; i++) {
@@ -16,6 +19,16 @@ router.get('/', async (req, res, next) => {
         x => !pantry.includes(x.ingredientName)
       )
       if (len.length <= 2) {
+        // OB/MS: an easier data model might be one array:
+        /*
+        [{
+          recipe: RECIPE_HERE,
+          ingredientsNeeded: INGREDIENTS_HERE
+        }, {
+          recipe: RECIPE_HERE,
+          ingredientsNeeded: INGREDIENTS_HERE
+        }]
+        */
         ingredientsArr.push(len)
         result.push(recipeToAdd[i])
       }

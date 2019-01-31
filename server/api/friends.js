22717f6c46fbaf324c4f3aa20cf10399dac177ad
RNAHVI <rnahvi@hotmail.com>
2019-01-31 14:17:34 -0800

    route to add a friend

--- server/api/friends.js
+++ server/api/friends.js
@@ -21,3 +21,20 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+// POST api/friends
+// We assume req.body contains an Id of a friend to add. E.g., {friendId:17}
+
+router.post('/', async (req, res, next) => {
+  try {
+    const friend = await User.create(req.body, {
+      include: [{model: User, as: 'friends', attributes: ['userName']}],
+      where: {
+        id: req.user.id
+      }
+    })
+    res.status(201).json(friend)
+  } catch (error) {
+    next(error)
+  }
+})

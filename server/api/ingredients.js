cffd7cac94aff4f3573fd751d109825bdd904984
Thomas Weidner <tomweidner103@gmail.com>
2019-02-04 08:57:32 -0800

    more tweaks

--- server/api/ingredients.js
+++ server/api/ingredients.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Ingredients} = require('../db/models')
+const {Ingredients, User, Recipes, Items} = require('../db/models')
 const Sequelize = require('sequelize')
 
 router.get('/', async (req, res, next) => {
@@ -14,6 +14,41 @@ router.get('/', async (req, res, next) => {
       limit: 10
     })
     res.json(suggestions)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//work in progress
+router.get('/ings', async (req, res, next) => {
+  try {
+    const Op = Sequelize.Op
+    const {pantryItems} = await User.findById(3, {
+      attributes: ['pantryItems']
+    })
+    let result = []
+    const recipes = await Recipes.findAll({
+      include: [
+        {
+          model: Items,
+          as: 'ingredientsIncluded',
+          where: {
+            ingredientName: {
+              [Op.like]: {[Op.any]: pantryItems}
+            }
+          }
+        }
+      ]
+    })
+    for (let i = 0; i < recipes.length; i++) {
+      const floor = Math.floor(
+        recipes[i].ingredientsIncluded.length / pantryItems.length * 100
+      )
+      if (floor > 60) {
+        result.push(recipes[i])
+      }
+    }
+    res.json(result)
   } catch (err) {
     next(err)
   }

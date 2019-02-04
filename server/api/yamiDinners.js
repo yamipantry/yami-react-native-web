a7793b31faf7f77f986e8e82d87b827ea699d8c5
Victor Marta <vicm159@gmail.com>
2019-02-03 21:57:41 -0800

    Yami Dinner backend 80 percent

---
+++ server/api/yamiDinners.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+const {yamiDinners} = require('../db/models')
+const GOOGLE_MAP_KEY = require('../../secrets')
+const googleMapsClient = require('@google/maps').createClient({
+  key: GOOGLE_MAP_KEY,
+  Promise: Promise
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    // there is going to be some logic as to how to get yami dinners
+    // close by for now well get all the yami dinners
+    const allDinners = await yamiDinners.findAll()
+    res.json(allDinners)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const location = await yamiDinners.geo(
+      req.body.streetName,
+      req.body.city,
+      req.body.state,
+      req.body.zip,
+      googleMapsClient
+    )
+    const yamiDinner = await yamiDinners.create({
+      ...req.body,
+      lat: location.lat,
+      lng: location.lng,
+      userId: req.user.id
+    })
+    res.json(yamiDinner)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
--- server/db/models/yamiDinners.js
+++ server/db/models/yamiDinners.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
-
+// not adding this but there appears to be a geometry data type for
+// postgres will need to research this later
 const yamiDinners = db.define('yamiDinners', {
   name: {
     type: Sequelize.STRING
@@ -8,24 +9,83 @@ const yamiDinners = db.define('yamiDinners', {
   description: {
     type: Sequelize.TEXT
   },
-  streeName: {
-    type: Sequelize.STRING
+  streetName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   city: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   state: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   zip: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
-  latitude: {
-    type: Sequelize.FLOAT
+  lat: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
-  longitude: {
-    type: Sequelize.FLOAT
+  lng: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  expir: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
   }
 })
+
+yamiDinners.geo = async function(
+  streetName,
+  city,
+  state,
+  zip,
+  googleMapsClient
+) {
+  const oldLocation = await this.findOne({
+    where: {
+      streetName: streetName,
+      city: city,
+      state: state,
+      zip: zip
+    },
+    attributes: ['lat', 'lng']
+  })
+  if (oldLocation) {
+    return {
+      lat: oldLocation.lat,
+      lng: oldLocation.lng
+    }
+  } else {
+    const location = await googleMapsClient
+      .geocode({
+        address: `${streetName}, ${city}, ${state} ${zip}`
+      })
+      .asPromise()
+    return location.json.results[0].geometry.location
+  }
+}
 
 module.exports = yamiDinners
